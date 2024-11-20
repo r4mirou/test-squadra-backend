0d2f7a68dbfd06dc8f268b0f4de2a23609fabb3e
r4mirou <ramirolima.dev@gmail.com>
2024-11-20 15:57:20 -0300

    chore: configurando helmet, compression e cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import helmet from 'helmet';
 import { Swagger } from './config/swagger/swagger';
 
 async function bootstrap() {
   const port = process.env.DEFAULT_HTTP_PORT ?? 3000;
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.use(helmet());
+  app.use(compression());
 
   Swagger.initalizer(app);
 
