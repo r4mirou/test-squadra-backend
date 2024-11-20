6a4260e5b10f1223c32dccb5860387b542cc90e5
r4mirou <ramirolima.dev@gmail.com>
2024-11-20 15:56:04 -0300

    chore: configurando swagger

---
+++ src/config/swagger/swagger.ts
@@ -0,0 +1,20 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+
+export class Swagger {
+  private static getConfig(): Omit<OpenAPIObject, 'paths'> {
+    const config = new DocumentBuilder()
+      .setTitle("Article System Doc's")
+      .setDescription('')
+      .setVersion('0.0.1')
+      .build();
+
+    return config;
+  }
+
+  static initalizer(app: NestExpressApplication) {
+    const config = this.getConfig();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`/docs`, app, document);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { Swagger } from './config/swagger/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.DEFAULT_HTTP_PORT ?? 3000;
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  Swagger.initalizer(app);
+
+  await app.listen(port, () =>
+    console.log(`Application is running on PORT: ${port}`),
+  );
 }
 bootstrap();
