1292c00264cc05ab7f64a093019e7f3d26f8f51e
r4mirou <ramirolima.dev@gmail.com>
2024-11-25 03:18:09 -0300

    chore: configurações de testes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
+  rootDir: './src',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+  testRegex: '.*\\.test\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
